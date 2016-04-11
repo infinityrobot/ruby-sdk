73de2c0adb58318c7a4a5e35a6fb99d0e43b66c4
Ritchie Blair <ritchie@infinityrobot.com>
2016-04-11 11:21:55 +1000

    :bulb: Delete any keys with empty values from generated list item properties

--- lib/riq/list.rb
+++ lib/riq/list.rb
@@ -152,14 +152,11 @@ module RIQ
     # specified list.
     # @param options [Hash] Hash of property options
     def list_item_properties(properties = {})
-      {
-        id: properties[:id],
-        list_id: @id,
-        name: @title,
-        contact_ids: [properties[:contact_ids]].flatten,
-        account_id: properties[:account_id],
-        field_values: coded_field_values(properties[:field_values])
-      }
+      props = { id: properties[:id], list_id: @id, name: @title,
+                contact_ids: [properties[:contact_ids]].flatten,
+                account_id: properties[:account_id],
+                field_values: coded_field_values(properties[:field_values]) }
+      props.delete_if { |_k, v| v.nil? || v.is_a?(Array) && v.compact.empty? }
     end
 
     # Convenience method for fetching or creating a listitem from the given list
